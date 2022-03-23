f9d89f541ac673bc25e1a6f1c784355c7e2cb3a1
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-23 23:03:18 +0900

    Create list_create.c

---
+++ utils/list_create.c
@@ -0,0 +1,12 @@
+t_stack	*list_create(void)
+{
+	t_stack	*tmp;
+
+	tmp = (t_stack *) malloc(sizeof(t_stack));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->first = NULL;
+	tmp->last = NULL;
+	tmp->size = 0;
+	return (tmp);
+}
