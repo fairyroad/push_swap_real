80eeb344bef7f09d9bf1552dfe6c621baef037f9
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-24 00:15:36 +0900

    Update ft_tab.c

--- sources/ft_tab.c
+++ sources/ft_tab.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../includes/push_swap.h"
 
 t_tab	convert_to_tab(t_stack *a)
 {
