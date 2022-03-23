47e57a5a78eec8c506002aa80c90ecd2ecd09aed
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-24 00:19:54 +0900

    Update ft_pa.c

--- sources/operations/ft_pa.c
+++ sources/operations/ft_pa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "../../includes/push_swap.h"
 
 void	pa(t_push *push)
 {
