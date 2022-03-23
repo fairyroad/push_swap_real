8d6811b760f774b493538230b45096f12cc42711
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-24 00:19:09 +0900

    Update list_create_node.c

--- sources/utils/list_create_node.c
+++ sources/utils/list_create_node.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "../../includes/push_swap.h"
 
 t_node	*list_create_node(void *content)
 {
