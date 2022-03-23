3d199a9caa0e092b6e9edde05ffb6d41f319982a
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-24 00:21:34 +0900

    Update ft_rrb.c

--- sources/operations/ft_rrb.c
+++ sources/operations/ft_rrb.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "../../includes/push_swap.h"
 
 void	rrb(t_push *push)
 {
