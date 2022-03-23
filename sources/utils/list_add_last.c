d0601bfc51f56a66bee5b559331898f4ab1d1331
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-24 00:18:26 +0900

    Update list_add_last.c

--- sources/utils/list_add_last.c
+++ sources/utils/list_add_last.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "../../includes/push_swap.h"
 
 void	list_add_last(t_stack *a, void *content)
 {
